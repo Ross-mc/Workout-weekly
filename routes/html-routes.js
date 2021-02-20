214ad1b13336993e82cdf0a3336a06edc8375483
Ross McIntyre <rossmci90@hotmail.co.uk>
2021-02-20 10:33:14 +0000

    Feat: added index route

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -7,10 +7,7 @@ const isAuthenticated = require("../config/middleware/isAuthenticated");
 module.exports = function(app) {
   app.get("/", (req, res) => {
     // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/members");
-    }
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
+    res.render("index", {});
   });
 
   app.get("/login", (req, res) => {
