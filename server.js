19de93b4c8fc64be329f085dac38c03c77ce820c
Ross McIntyre <rossmci90@hotmail.co.uk>
2021-02-23 00:23:22 +0000

    fix: eslint fixing

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const session = require("express-session");
 // Requiring passport as we've configured it
 const passport = require("./config/passport");
 const exphbs = require("express-handlebars");
-require('dotenv').config();
+require("dotenv").config();
 
 // Setting up port and requiring models for syncing
 const PORT = process.env.PORT || 8080;
